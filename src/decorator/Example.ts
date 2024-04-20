cdb460a41510de8bab3b24e80b6a66626bafb7f3
jeongho <pjhpjh0805@naver.com>
2024-04-20 14:36:09 +0900

    feat: 클래스 데코레이터 작성

--- src/decorator/Example.ts
+++ src/decorator/Example.ts
@@ -1,19 +1,33 @@
-function first() {
-    console.log("first(): factory evaluated");
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        console.log("first(): called");
-    };
+/**
+ * 여기서의 T는 생성자 함수를 뜻한다.
+ */
+function LogMethods<T extends { new(...args: any[]): {} }>(constructor: T) {
+    return class extends constructor {
+        constructor(...args: any[]) {
+            super(...args); // 원본 클래스의 생성자 함수 호출
+            Object.getOwnPropertyNames(constructor.prototype).forEach(property => {
+                if (typeof (this as any)[property] === 'function' && property !== 'constructor') { // 생성자 제외 전체 메서드에 적용
+                    const original = (this as any)[property];
+                    (this as any)[property] = function (...args: any[]) {
+                        console.log("method call start");
+                        const result = original.apply(this, args);
+                        console.log(`method call end`);
+                        return result;
+                    }
+                }
+            });
+        }
+    }
 }
 
-function second() {
-    console.log("second(): factory evaluated");
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        console.log("second(): called");
-    };
-}
+@LogMethods
+export class Example {
 
-export class ExampleClass {
-    @first()
-    @second()
-    method() {}
+    constructor() {
+        console.log("생성자 함수 실행");
+    }
+
+    method1() {
+        console.log("메서드 1 실행");
+    }
 }
--- test/decorator.test.ts
+++ test/decorator.test.ts
@@ -1,9 +1,8 @@
-import {ExampleClass} from "../src/decorator/Example";
+import {Example} from "../src/decorator/Example";
 
 describe('test', () => {
     test('decorator test', () => {
-        const example = new ExampleClass();
-        example.method();
-        expect(200).toBe(200);
+        const example = new Example();
+        example.method1();
     });
 });
